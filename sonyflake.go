809c515cc5acc48646233f547226e8ad0e07841f
Yoshiyuki Mineo <Yoshiyuki.Mineo@jp.sony.com>
2022-08-12 18:38:40 +0900

    Introduce GitHub Actions (#34)
    * Introduce GitHub Actions
    
    * gofmt

--- sonyflake.go
+++ sonyflake.go
@@ -1,9 +1,10 @@
 // Package sonyflake implements Sonyflake, a distributed unique ID generator inspired by Twitter's Snowflake.
 //
 // A Sonyflake ID is composed of
-//     39 bits for time in units of 10 msec
-//      8 bits for a sequence number
-//     16 bits for a machine id
+//
+//	39 bits for time in units of 10 msec
+//	 8 bits for a sequence number
+//	16 bits for a machine id
 package sonyflake
 
 import (
