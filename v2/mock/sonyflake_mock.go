7b636bf93a7006cff8aaaf9fadb3dfca2c50e3f3
Yoshiyuki Mineo <Yoshiyuki.Mineo@sony.com>
2025-05-04 17:54:58 +0900

    docs(v2/mock): improve comments and docstrings for mock implementations

--- v2/mock/sonyflake_mock.go
+++ v2/mock/sonyflake_mock.go
@@ -1,6 +1,5 @@
-// Package mock offers implementations of interfaces defined in types.go
-// This allows complete control over input / output for any given method that consumes
-// a given type
+// Package mock offers mock implementations of interfaces defined in types.go.
+// This allows complete control over input / output for any given method that consumes a given type.
 package mock
 
 import (
@@ -10,7 +9,7 @@ import (
 	"github.com/sony/sonyflake/v2/types"
 )
 
-// NewSuccessfulInterfaceAddrs returns a single private IP address
+// NewSuccessfulInterfaceAddrs returns a single private IP address.
 func NewSuccessfulInterfaceAddrs() types.InterfaceAddrs {
 	ifat := make([]net.Addr, 0, 1)
 	ifat = append(ifat, &net.IPNet{IP: []byte{192, 168, 0, 1}, Mask: []byte{255, 0, 0, 0}})
@@ -20,14 +19,14 @@ func NewSuccessfulInterfaceAddrs() types.InterfaceAddrs {
 	}
 }
 
-// NewFailingInterfaceAddrs returns an error
+// NewFailingInterfaceAddrs returns an error.
 func NewFailingInterfaceAddrs() types.InterfaceAddrs {
 	return func() ([]net.Addr, error) {
 		return nil, fmt.Errorf("test error")
 	}
 }
 
-// NewFailingInterfaceAddrs returns an empty slice of addresses
+// NewNilInterfaceAddrs returns an empty slice of addresses.
 func NewNilInterfaceAddrs() types.InterfaceAddrs {
 	return func() ([]net.Addr, error) {
 		return []net.Addr{}, nil
