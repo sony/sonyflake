901da0b0224eafce36eec4797a39eaad139e9671
Yoshiyuki Mineo <Yoshiyuki.Mineo@sony.com>
2025-05-04 18:02:49 +0900

    docs(types): unify and clarify package and type docstrings for types.go in v1 and v2

--- types/types.go
+++ types/types.go
@@ -1,8 +1,8 @@
-// Package Types defines type signatures used throughout SonyFlake. This allows for
-// fine-tuned control over imports, and the ability to mock out imports as well
+// Package types defines type signatures used throughout sonyflake.
+// This provides the ability to mock out imports.
 package types
 
 import "net"
 
-// InterfaceAddrs defines the interface used for retrieving network addresses
+// InterfaceAddrs defines the interface used for retrieving network addresses.
 type InterfaceAddrs func() ([]net.Addr, error)
